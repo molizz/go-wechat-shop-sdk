5cfe055d3abc0108b5de648db04870d049e6ad48
moli <molix2@outlook.com>
2021-06-09 10:57:27 +0800

    add: test

--- register_test.go
+++ register_test.go
@@ -15,3 +15,13 @@ func TestRegister_Check(t *testing.T) {
 	_, err = r.Check()
 	assert.Nil(t, err)
 }
+
+func TestRegister_Apply(t *testing.T) {
+	at, err := NewAccessToken(APPID, SECRETE).Get()
+	assert.Nil(t, err)
+
+	r := NewRegister(at)
+
+	_, err = r.Apply()
+	assert.Nil(t, err)
+}
